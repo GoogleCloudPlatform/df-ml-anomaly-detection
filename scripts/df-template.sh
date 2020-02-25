5b42839254d983f66a92e23452f43f143986b293
Masud <masudhasan@google.com>
2020-02-25 15:45:36 -0500

    tested automatic scripts

--- scripts/df-template.sh
+++ scripts/df-template.sh
@@ -11,19 +11,6 @@
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
-# limitations under the License.#!/usr/bin/env bash
-# Copyright 2019 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
 # limitations under the License.
  
 set -x 
@@ -39,7 +26,7 @@ OUTLIER_TABLE_SPEC="${PROJECT_ID}:${BQ_DATASET}.outlier_data"
 DEID_TEMPLATE_NAME="projects/${PROJECT_ID}/deidentifyTemplates/deid-template"
 SUBSCRIPTION_ID="projects/${PROJECT_ID}/subscriptions/${SUBSCRIPTION_ID}"
 BATCH_SIZE="100"
-INPUT_FILE_PATTERN="gs://wesp-flow-logs/wesp_flow_*.json"
+INPUT_FILE_PATTERN="gs://next-demo-2020-net-flow-log/wesp-flow-logs/wesp_flow_*.json"
 # publicly hosted image
 DYNAMIC_TEMPLATE_BUCKET_SPEC="gs://wesp-flow-logs/dynamic_template_secure_log_aggr_template.json"
 JOB_NAME="netflow-anomaly-detection-`date +%Y%m%d-%H%M%S-%N`"
@@ -49,14 +36,14 @@ TEMP_LOCATION="gs://${DATA_STORAGE_BUCKET}/temp"
 PARAMETERS_CONFIG='{  
    "jobName":"'$JOB_NAME'",
    "parameters":{  
-      "streaming": "true",
-	  "enableStreamingEngine": "false",
-	  "autoscalingAlgorithm": "NONE",
-      "workerMachineType": "n1-standard-8",
-      "numWorkers": "50",
-      "maxNumWorkers": "50",
-	  "region": "us-central1",
-      "tableSpec":"'$TABLE_SPEC'",
+     	 "streaming": "true",
+	 "enableStreamingEngine": "false",
+	 "autoscalingAlgorithm": "NONE",
+    	 "workerMachineType": "n1-standard-8",
+     	 "numWorkers": "50",
+    	 "maxNumWorkers": "50",
+	 "region": "us-central1",
+      	 "tableSpec":"'$TABLE_SPEC'",
 	  "batchFrequency":"2",
 	  "clusterQuery":"'$CLUSTER_QUERY'",
 	  "outlierTableSpec":"'$OUTLIER_TABLE_SPEC'",
@@ -66,7 +53,8 @@ PARAMETERS_CONFIG='{
 	  "deidTemplateName":"'$DEID_TEMPLATE_NAME'",
 	  "batchSize":"'$BATCH_SIZE'",
 	  "customGcsTempLocation":"'$TEMP_LOCATION'",
-	  "inputFilePattern":"'$INPUT_FILE_PATTERN'"  
+	  "inputFilePattern":"'$INPUT_FILE_PATTERN'" ,
+	   
 	}
 }'
 DF_API_ROOT_URL="https://dataflow.googleapis.com"
